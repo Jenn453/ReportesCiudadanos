f65d09674203c0c832c35a79b86c6a1ee6d0eb57
Jordy Diaz <diaz.jordyb@gmail.com>
2025-09-27 17:07:55 -0500

    Scrum-14: Creacion de clase para permisos rol

--- src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
+++ src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
@@ -1,4 +1,23 @@
 package co.edu.uniquindio.proyecto;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
 public class ProyectoApplication {
+
+    public static void main(String[] args) {
+
+        /*Usuario usuario = new Usuario(........);
+        usuarioRepository.save(usuario); //db.usuarios.insertOne(......)
+        usuarioRepository.remove()
+        usuarioRepository.findByEmail() // db.usuarios.find({email: "sdsdsdssd"})
+        usuarioRepository.findAll()*/
+
+        SpringApplication.run(ProyectoApplication.class, args);
+    }
+
+
 }
