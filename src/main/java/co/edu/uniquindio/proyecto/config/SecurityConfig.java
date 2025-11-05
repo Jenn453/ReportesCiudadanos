6d7527ead8d565af5083246ead8c8ee0b6e5f8bb
sara-bedoya <sharaj.bedoyap@uqvirtual.edu.co>
2025-11-05 00:36:59 -0500

    se actualiza el cors para despliegue en firebase

--- src/main/java/co/edu/uniquindio/proyecto/config/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/proyecto/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         // Configura las políticas de CORS para permitir solicitudes desde el frontend
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://reporte-ciudadano-front.web.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
